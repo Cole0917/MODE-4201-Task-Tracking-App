a27f2f2f4c622f06a3c05cdcdf74e76df1a52c43
Cole0917 <colejb17@gmail.com>
2023-12-14 00:00:11 -0500

    Implemented home, manage tasks, statistics, settings, and calendar pages. There is functionality in the Manage Tasks page for adding a task, editing the task name, deleting, and marking as complete.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,295 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
+class TaskModel {
+  String name;
+  bool isComplete;
+
+  TaskModel({required this.name, this.isComplete = false});
+}
+
+class TaskManager {
+  static final TaskManager _instance = TaskManager._internal();
+
+  factory TaskManager() {
+    return _instance;
+  }
+
+  TaskManager._internal();
+
+  List<TaskModel> tasks = [];
+}
+
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          titleTextStyle: GoogleFonts.pacifico(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
         ),
       ),
+      home: const MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    const HomePage(),
+    const ManageTasksPage(),
+    const StatisticsPage(),
+    const SettingsPage(),
+    const CalendarPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Task Tracker'),
+      ),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assignment),
+            label: 'Manage Tasks',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.insert_chart),
+            label: 'Statistics',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Calendar',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Welcome to Task Tracker!',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Organize your tasks and boost productivity with our Task Tracker app.',
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ManageTasksPage extends StatefulWidget {
+  const ManageTasksPage({Key? key}) : super(key: key);
+
+  @override
+  _ManageTasksPageState createState() => _ManageTasksPageState();
+}
+
+class _ManageTasksPageState extends State<ManageTasksPage> {
+  TextEditingController _taskController = TextEditingController();
+  TaskManager taskManager = TaskManager();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Manage Tasks'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _taskController,
+              decoration: InputDecoration(
+                hintText: 'Enter a new task',
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: () {
+                    setState(() {
+                      taskManager.tasks
+                          .add(TaskModel(name: _taskController.text));
+                      _taskController.clear();
+                    });
+                  },
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: taskManager.tasks.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(taskManager.tasks[index].name),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.edit),
+                        onPressed: () {
+                          _editTask(index);
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () {
+                          _deleteTask(index);
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.alarm),
+                        onPressed: () {
+                          // Implement set reminder functionality
+                          // You can use a date picker or show a dialog to set the reminder
+                        },
+                      ),
+                      Checkbox(
+                        value: taskManager.tasks[index].isComplete,
+                        onChanged: (value) {
+                          setState(() {
+                            taskManager.tasks[index].isComplete = value!;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _editTask(int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Edit Task'),
+          content: TextField(
+            controller:
+                TextEditingController(text: taskManager.tasks[index].name),
+            onChanged: (text) {
+              taskManager.tasks[index].name = text;
+            },
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  Navigator.of(context).pop();
+                });
+              },
+              child: Text('Save'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _deleteTask(int index) {
+    setState(() {
+      taskManager.tasks.removeAt(index);
+    });
+  }
+
+  @override
+  void dispose() {
+    _taskController.dispose();
+    super.dispose();
+  }
+}
+
+class StatisticsPage extends StatelessWidget {
+  const StatisticsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Statistics Page'),
+    );
+  }
+}
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Settings Page'),
+    );
+  }
+}
+
+class CalendarPage extends StatelessWidget {
+  const CalendarPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Calendar Page'),
     );
   }
 }
