42eaabc9394e2111ba2a0a61ff54344e1dcd9e0a
Bhargav Patel <bhargav.patel1@dcmail.ca>
2023-12-15 18:45:20 -0500

    added new button to help users in the settings page

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -19,7 +19,7 @@ class _MainScreenState extends State<MainScreen> {
     const HomePage(),
     const ManageTasksPage(),
     const StatisticsPage(),
-    const SettingsPage(),
+    SettingsPage(),
     const CalendarPage(),
   ];
 
--- lib/screens/settings_page.dart
+++ lib/screens/settings_page.dart
@@ -1,9 +1,22 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-class SettingsPage extends StatelessWidget {
-  const SettingsPage({Key? key}) : super(key: key);
 
+class SettingsPage extends StatelessWidget {
+   SettingsPage({Key? key}) : super(key: key);
+
+ButtonStyle buttonHelp = ElevatedButton.styleFrom(
+    minimumSize: Size(100, 50),
+    backgroundColor: Colors.lightBlue[50],
+    elevation: 0,
+    
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(30))
+    )
+  );
+  
   @override
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
@@ -26,9 +39,31 @@ class SettingsPage extends StatelessWidget {
           ],
         ),
       ),
-      body: const Center(
-        child: Text('Settings Page'),
-      ),
+      body: Column(
+        children: [
+          ElevatedButton(
+            style: buttonHelp,
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context){
+                  return AlertDialog(
+                    title: const Text("How to use Application"),
+                    content: const Text("To use the application, first go to the Manage task page. Then enter the task name in the place Enter a new task. When you are ready with the name, press + sign and your task will be added. And in the added task, if you want to edit, you can do that, you can delete the task, you can even tick mark the task after you are done with it."),
+                    actions: [
+                      TextButton( 
+                        onPressed: () => Navigator.of(context).pop(true),
+                        child: const Text('close'),
+                      )
+                    ],
+                  );
+                }
+              );
+              },
+            child: Text('Help'),)
+        ],
+      ) 
+       
     );
   }
 }
